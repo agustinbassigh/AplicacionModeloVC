d6a95987eba1a3606be8fb8953e29c52fd2c1736
agustinbassigh <agustin.bassi@hotmail.com>
2018-08-10 21:31:08 -0300

    Add project files.

---
+++ AplicacionModeloVC/Models/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionModeloVC.Models
+{
+    [Table("Persona")]
+    public class Persona
+    {
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("Nombre")]
+        public string Nombre { get; set; }
+
+        [Column("DNI")]
+        public int DNI { get; set; }
+
+        [Column("Promedio")]
+        public decimal Promedio { get; set; }
+
+        [Column("Activo")]
+        public bool Activo { get; set; }
+    }
+}
